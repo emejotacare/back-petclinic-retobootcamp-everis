150c1246a3810f40b614978990c446685b282adc
apozogar <albertopozogarcia@gmail.com>
2018-09-28 11:05:07 +0200

    Interfaz del repository/OfertaRepository.java

---
+++ src/main/java/com/bootcamp/petclinic/repository/OfertaRepository.java
@@ -0,0 +1,9 @@
+package com.bootcamp.petclinic.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bootcamp.petclinic.entity.Oferta;
+
+public interface OfertaRepository extends JpaRepository<Oferta,Integer>{
+
+}
