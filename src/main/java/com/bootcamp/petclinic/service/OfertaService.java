3ba0bc3ae0974400ef016208346ff06c0019ba6a
Alberto <alberto1sevilla@hotmail.com>
2018-09-28 11:32:22 +0200

    servicio

--- src/main/java/com/bootcamp/petclinic/service/OfertaService.java
+++ src/main/java/com/bootcamp/petclinic/service/OfertaService.java
@@ -1,5 +1,18 @@
 package com.bootcamp.petclinic.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.bootcamp.petclinic.entity.Oferta;
+
 public interface OfertaService {
+	
+	Optional<Oferta> findById(int id);
+	
+	List<Oferta> findAll();
+	
+	Oferta saveOferta(Oferta oferta);
+	
+	void deleteOferta(int id);
 
 }
--- src/main/java/com/bootcamp/petclinic/service/impl/OfertaServiceImpl.java
+++ src/main/java/com/bootcamp/petclinic/service/impl/OfertaServiceImpl.java
@@ -1,7 +1,13 @@
 package com.bootcamp.petclinic.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bootcamp.petclinic.entity.Oferta;
+import com.bootcamp.petclinic.repository.OfertaRepository;
 import com.bootcamp.petclinic.service.OfertaService;
 
 @Service
@@ -9,9 +15,23 @@ public class OfertaServiceImpl implements OfertaService{
 	
 	@Autowired
 	private OfertaRepository ofertaRepo;
-
-	public OfertaServiceImpl() {
-		// TODO Auto-generated constructor stub
+	
+	public Optional<Oferta> findById(int id) {
+		return this.ofertaRepo.findById(id);
+	}
+	
+	public List<Oferta> findAll() {
+		return this.ofertaRepo.findAll();
+	}
+	
+	public Oferta saveOferta(Oferta oferta) {
+		return this.ofertaRepo.save(oferta);
 	}
 
+	public void deleteOferta(int id) {
+		this.ofertaRepo.deleteById(id);
+	}
+
+
+	
 }
