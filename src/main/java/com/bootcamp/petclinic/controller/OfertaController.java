ce349ec0c960524ecd513809845c93a192dba8b8
apozogar <albertopozogarcia@gmail.com>
2018-09-28 12:55:30 +0200

    list, save y delete

--- src/main/java/com/bootcamp/petclinic/controller/OfertaController.java
+++ src/main/java/com/bootcamp/petclinic/controller/OfertaController.java
@@ -1,12 +1,14 @@
 package com.bootcamp.petclinic.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -36,10 +38,31 @@ public class OfertaController {
 			return ofertaService.saveOferta(oferta);
 		}
 		
+		/*Actualizar oferta
+		@PutMapping("/ofertas/{id}")
+		@ResponseStatus(HttpStatus.CREATED)
+		public Oferta update(@RequestBody Oferta oferta, @PathVariable int id) {
+
+			Optional<Oferta> ofertaCurrent = ofertaService.findById(id);
+			
+			if(ofertaCurrent.isPresent()) {
+				Oferta ofertas = ofertaCurrent.get();
+				ofertas.setDescuento(oferta.getDescuento());
+				ofertas.setDetalle(oferta.getDetalle());
+				ofertas.setFechaCaducidad(oferta.getFechaCaducidad());
+				ofertas.setTitulo(oferta.getTitulo());
+			}
+			
+			return ofertaService.saveOferta(ofertaCurrent);
+		}
+		*/
+		
 		//Eliminar oferta
 		@DeleteMapping("/ofertas/{id}")
 		@ResponseStatus(HttpStatus.NO_CONTENT)
 		public void delete(@PathVariable int id) {
 			ofertaService.deleteOferta(id);
 		}
+		
+		
 }
